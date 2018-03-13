21f0ba63a7e5bf7889f8043957ecbab0f5e95fc7
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-03-13 13:09:17 -0500

    Refactor NotFoundError handling for clarity

--- sql/src/main/kotlin/io/github/codebandits/beak/entityOperations.kt
+++ sql/src/main/kotlin/io/github/codebandits/beak/entityOperations.kt
@@ -1,37 +1,32 @@
 package io.github.codebandits.beak
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.data.Try
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 fun <ID : Comparable<ID>, T : Entity<ID>> EntityClass<ID, T>.allOrError(): Either<DataAccessError, List<T>> =
-        Try { all().toList() }.handleDataAccessErrors()
+    Try { all().toList() }.mapFailureToDataAccessError()
 
 fun <ID : Comparable<ID>, T : Entity<ID>> EntityClass<ID, T>.newOrError(init: T.() -> Unit): Either<DataAccessError, T> =
-        Try { new(init).also { TransactionManager.current().commit() } }.handleDataAccessErrors()
+    Try { new(init).also { TransactionManager.current().commit() } }.mapFailureToDataAccessError()
 
 fun <ID : Comparable<ID>, T : Entity<ID>> EntityClass<ID, T>.findByIdOrError(id: ID): Either<DataAccessError, T> =
-        Try { findById(id) }
-                .catchEntityErrors()
-                .handleDataAccessErrors()
-
-private fun <T> Try<T?>.catchEntityErrors(): Try<T> {
-    return flatMap {
-        if (it == null) {
-            Try.raise(Throwable("Not found"))
-        } else {
-            Try.pure(it)
+    Try { findById(id) }
+        .mapFailureToDataAccessError()
+        .flatMap {
+            when (it) {
+                null -> Either.left(DataAccessError.EntityError.NotFoundError(Throwable("Not found")))
+                else -> Either.right(it)
+            }
         }
-    }
-}
 
-private fun <T> Try<T>.handleDataAccessErrors(): Either<DataAccessError, T> = toEither().mapLeft {
+private fun <T> Try<T>.mapFailureToDataAccessError(): Either<DataAccessError, T> = toEither().mapLeft {
     when {
-        it.cause is java.net.ConnectException -> DataAccessError.SystemError.ConnectionError(it)
+        it.cause is java.net.ConnectException      -> DataAccessError.SystemError.ConnectionError(it)
         it.message == "No transaction in context." -> DataAccessError.SystemError.TransactionError(it)
-        it.message == "Not found" -> DataAccessError.EntityError.NotFoundError(it)
-        else -> throw UnexpectedException(it)
+        else                                       -> throw UnexpectedException(it)
     }
 }
