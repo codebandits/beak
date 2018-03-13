4cee3d0cd02eb73669aeb971468ef0aa6d9ab3b7
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-03-13 01:28:39 -0500

    Reorganize tests to focus on one function each

---
+++ sql/src/test/kotlin/io/github/codebandits/beak/NewOrErrorTest.kt
@@ -0,0 +1,48 @@
+package io.github.codebandits.beak
+
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class NewOrErrorTest : TestWithDatabase() {
+
+    @Test
+    fun `should create a record in the database`() {
+        transaction {
+            FeatherEntity.newOrError {}.assertRight()
+        }
+
+        transaction {
+            assertEquals(1, FeatherEntity.allOrError().assertRight().count())
+        }
+    }
+
+    @Test
+    fun `should save the data into a record in the database`() {
+        transaction {
+            val feather = FeatherEntity.newOrError {
+                type = "contour"
+            }.assertRight()
+
+            assertEquals("contour", feather.type)
+        }
+    }
+
+    @Test
+    fun `should return a failure when the database cannot connect`() {
+        server.stop()
+
+        transaction {
+            val actualError: DataAccessError = FeatherEntity.newOrError {}.assertLeft()
+
+            assertEquals(DataAccessError.SystemError.ConnectionError::class, actualError::class)
+        }
+    }
+
+    @Test
+    fun `should return a failure when there is no transaction`() {
+        val actualError: DataAccessError = FeatherEntity.newOrError {}.assertLeft()
+
+        assertEquals(DataAccessError.SystemError.TransactionError::class, actualError::class)
+    }
+}
