f9d192834a8a9b3cce30a1bbf2e063f8e87dc68f
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-03-15 18:24:55 -0600

    Configure the Gradle Bintray Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import com.jfrog.bintray.gradle.BintrayUploadTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     project.apply { from("gradle/scripts/versions.gradle.kts") }
 
     val kotlinVersion: String by extra
+    val bintrayPluginVersion: String by extra
 
     repositories {
         jcenter()
@@ -11,6 +13,7 @@ buildscript {
 
     dependencies {
         classpath(kotlin(module = "gradle-plugin", version = kotlinVersion))
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:$bintrayPluginVersion")
     }
 }
 
@@ -19,6 +22,7 @@ allprojects {
         from("${rootProject.projectDir}/gradle/scripts/versions.gradle.kts")
         plugin("org.jetbrains.kotlin.jvm")
         plugin("maven-publish")
+        plugin("com.jfrog.bintray")
     }
 
     val jvmTarget: String by extra
@@ -41,6 +45,14 @@ allprojects {
 
         testLogging {
             events("passed", "skipped", "failed")
+        }
+    }
+
+    tasks.withType<BintrayUploadTask> {
+        doFirst {
+            when (version) {
+                !is String, "unspecified" -> throw Throwable("${project.name} version is not specified (was \"$version\")")
+            }
         }
     }
 }
