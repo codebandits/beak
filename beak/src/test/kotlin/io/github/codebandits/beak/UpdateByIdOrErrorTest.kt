cd0d8106407fd07e645ef42952ef4c837f33ee4d
Leonhardt Koepsell <leonhardt@koepsell.io>
2018-03-21 18:51:25 -0500

    Various update improvements
    - Fail updateWhere when no entities match
    - Add KDoc for update methods
    - Add missing transaction tests
    
    Co-authored-by: Leonhardt Koepsell <leonhardt@koepsell.io>
    Co-authored-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- beak/src/test/kotlin/io/github/codebandits/beak/UpdateByIdOrErrorTest.kt
+++ beak/src/test/kotlin/io/github/codebandits/beak/UpdateByIdOrErrorTest.kt
@@ -1,6 +1,7 @@
 package io.github.codebandits.beak
 
-import org.jetbrains.exposed.dao.EntityID
+import io.github.codebandits.beak.DataAccessError.QueryError.NotFoundError
+import io.github.codebandits.beak.DataAccessError.SystemError.ConnectionError
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
@@ -25,42 +26,24 @@ abstract class UpdateByIdOrErrorTest : TestWithDatabase() {
 
     @Test
     fun `should update an entity from the table`() {
-        transaction {
+        val id = transaction {
             val expectedEntity = FeatherEntity.newOrError {
                 type = "contour"
             }.assertRight()
 
-            val id: EntityID<Long> = expectedEntity.id
+            val id = expectedEntity.id.value
 
-            FeatherEntity.updateByIdOrError(id.value) {
+            FeatherEntity.updateByIdOrError(id) {
                 type = "contour-updated"
             }.assertRight()
 
+            id
+        }
+
+        transaction {
             assertEquals(
                 "contour-updated",
-                FeatherEntity.findByIdOrError(id.value).assertRight().type
-            )
-        }
-    }
-
-    @Test
-    fun `should update multiple entities that returns from query from the table`() {
-        transaction {
-            FeatherEntity.newOrError {
-                type = "contour"
-            }.assertRight()
-
-            FeatherEntity.newOrError {
-                type = "contour"
-            }.assertRight()
-
-            FeatherEntity.updateOrError({ FeatherTable.type eq "contour" }) {
-                type = "contour-updated"
-            }.assertRight()
-
-            assertEquals(
-                2,
-                FeatherEntity.findOrError({ FeatherTable.type eq "contour-updated" }).assertRight().count()
+                FeatherEntity.findByIdOrError(id).assertRight().type
             )
         }
     }
@@ -70,18 +53,20 @@ abstract class UpdateByIdOrErrorTest : TestWithDatabase() {
         databaseConfiguration.interruptDatabase()
 
         transaction {
-            val actualError: DataAccessError = FeatherEntity.updateByIdOrError(0L) {
-                type = "should not happen"
-            }.assertLeft()
-
-            assertEquals(DataAccessError.SystemError.ConnectionError::class, actualError::class)
+            assertEquals(
+                ConnectionError::class,
+                FeatherEntity.updateByIdOrError(0L) { type = "should not happen" }.assertLeft()::class
+            )
         }
     }
 
     @Test
     fun `should return a failure when there is no entities to update`() {
-        val actualError: DataAccessError = FeatherEntity.updateByIdOrError(0L) { type = "error" }.assertLeft()
-
-        assertEquals(DataAccessError.SystemError.TransactionError::class, actualError::class)
+        transaction {
+            assertEquals(
+                NotFoundError::class,
+                FeatherEntity.updateByIdOrError(0L) { type = "error" }.assertLeft()::class
+            )
+        }
     }
 }
